0e9668f4a9f0288ede7e249b65814e952b62da05
unknown <jordanmbush@gmail.com>
2018-02-06 11:34:55 -0700

    first commit

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Route, Switch, Link } from 'react-router-dom'
+import Home from './Home/Home';
+import About from './About/About';
+import People from './People/People';
 
 class App extends Component {
   render() {
     return (
       <div>
         This is App.js
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/people">People</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/people" component={People} />
+        </Switch>
       </div>
     );
   }
